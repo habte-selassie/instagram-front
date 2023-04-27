96b711d813dbe26683adb8cce665c818465ac436
Habte Selassie <habtuchelsea1000@gmail.com>
2023-04-27 18:53:07 +0300

    add icons in posted.js

--- src/Home/Posted.js
+++ src/Home/Posted.js
@@ -1,10 +1,25 @@
 import './Posted.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+///import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
 const WhenPosted = () => {
     return ( 
         <div>
             <div id="post-box">
 
             </div>
+            <div>
+            <h1>Icon</h1>
+
+            <FontAwesomeIcon icon={['fas', 'heart']} />
+            <FontAwesomeIcon icon={['fas', 'comment']} flip="horizontal" />
+            <FontAwesomeIcon icon={['fas', 'arrow-up-from-bracket']} />
+            <FontAwesomeIcon icon={['fas', 'bookmark']} />
+
+
+            </div>
+            
+
         </div>
      );
 }
